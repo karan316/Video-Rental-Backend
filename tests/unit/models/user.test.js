00ac690e8b2fc6038534787c802b12d855105cb1
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-04-29 17:55:12 +0530

    added unit testing to generateAuthToken()

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../../../models/user");
+const config = require("config");
+const mongoose = require("mongoose");
+describe("user.genrerateAuthToken", () => {
+    it("should return a valid JWT", () => {
+        const payload = {
+            // toHexString is because the id is stored as an array of numbers. When auth token is generated it gives back string representation.
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true,
+        };
+        const user = new User(payload);
+        const token = user.generateAuthToken();
+        const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+        expect(decoded).toMatchObject(payload);
+    });
+});
