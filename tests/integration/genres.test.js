b93123ab5f20cf30050f6af2a0bda7672305650f
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-04-30 19:01:41 +0530

    added auth unit and integration test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
 const { User } = require("../../models/user");
+const mongoose = require("mongoose");
 let server;
 
 describe("/api/genres", () => {
@@ -30,7 +31,13 @@ describe("/api/genres", () => {
 
     describe("GET /:id", () => {
         it("should return 404 if invalid id is passed", async () => {
-            const res = await request(server).get(`/api/genres/1`);
+            const res = await request(server).get("/api/genres/1");
+            expect(res.status).toBe(404);
+        });
+
+        it("should return 404 if no genre with given id exists", async () => {
+            const id = mongoose.Types.ObjectId();
+            const res = await request(server).get(`/api/genres/${id}`);
             expect(res.status).toBe(404);
         });
 
@@ -46,50 +53,54 @@ describe("/api/genres", () => {
 
     describe("POST /", () => {
         //Define the happy path and in each test we change one parameter that clearly aligns with the name of the test.
+        let token;
+        let name;
+
+        // refactoring to reduce the lines of code..duh
+        const exec = () => {
+            // awaiting is not necessary because we are immediately returning
+            return request(server)
+                .post("/api/genres")
+                .set("x-auth-token", token)
+                .send({ name });
+        };
+
+        beforeEach(() => {
+            //set the values for the happy path
+            // assign the token before every test
+            token = new User().generateAuthToken();
+            name = "genre1";
+        });
 
         it("should return a 401 if client is not logged in", async () => {
-            const res = await request(server)
-                .post("/api/genres")
-                .send({ name: "genre1" });
+            // set token to null because user should not be logged in
+            token = "";
+            const res = await exec();
 
             expect(res.status).toBe(401);
         });
+
         it("should return 400 if genre is less than 3 characters", async () => {
-            const token = new User().generateAuthToken();
-            const res = await request(server)
-                .post("/api/genres")
-                .set("x-auth-token", token)
-                .send({ name: "12" });
-
+            name = "12";
+            const res = await exec();
             expect(res.status).toBe(400);
         });
+
         it("should return 400 if genre is more than 50 characters", async () => {
-            const token = new User().generateAuthToken();
             // create a string of 51 characters
-            const name = new Array(52).join("a");
-            const res = await request(server)
-                .post("/api/genres")
-                .set("x-auth-token", token)
-                .send({ name: name });
-
+            name = new Array(52).join("a");
+            const res = await exec();
             expect(res.status).toBe(400);
         });
+        // HAPPY PATH. use this while refactoring
         it("should save the genre if it is valid", async () => {
-            const token = new User().generateAuthToken();
-            const res = await request(server)
-                .post("/api/genres")
-                .set("x-auth-token", token)
-                .send({ name: "genre1" });
-
+            await exec();
             const genre = await Genre.find({ name: "genre1" });
             expect(genre).not.toBeNull();
         });
+
         it("should return the genre if it is valid", async () => {
-            const token = new User().generateAuthToken();
-            const res = await request(server)
-                .post("/api/genres")
-                .set("x-auth-token", token)
-                .send({ name: "genre1" });
+            const res = await exec();
             expect(res.body).toHaveProperty("_id");
             expect(res.body).toHaveProperty("name", "genre1");
         });
