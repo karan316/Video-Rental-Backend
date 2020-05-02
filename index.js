12f1043e773ef858d627cf57b69a7083a118fb8f
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-05-02 23:11:24 +0530

    added cors

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const app = express();
 require("./startup/logging")(); // this should be required before everything else
 require("./startup/routes")(app); // routes returns a function. we call it with the app object
 require("./startup/db")();
+require("./startup/cors")(app);
 require("./startup/config");
 require("./startup/validation");
 require("./startup/prod")(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3900;
 
 const server = app.listen(port, () =>
     winston.info(`Listening on port ${port}`)
