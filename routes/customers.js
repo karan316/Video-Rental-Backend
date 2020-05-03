090ff5b2e34fd41d9dad46a91d9e223060ea77d1
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-05-03 10:24:29 +0530

    added vidly_db connection string

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Customer, validateCustomer } = require("../models/customer");
+
 router.get("/", async (req, res) => {
     const customers = await Customer.find().sort("name");
     res.send(customers);
